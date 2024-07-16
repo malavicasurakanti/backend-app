74519745a1fee22edd5695bf5cff5c6ed956ca25
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-16 19:05:43 +0200

    Update server.js core update

--- server.js
+++ server.js
@@ -26,7 +26,17 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
-app.use(cors());
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
+app.use(cors({
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
