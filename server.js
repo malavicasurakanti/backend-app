33022390378d4c803c7bf087ed0cef5c29669a33
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-19 17:31:33 +0200

    Update server.js

--- server.js
+++ server.js
@@ -46,6 +46,13 @@ app.use(cors({
 }));
 
 app.options('*', cors(corsOptions)); // Handle OPTIONS requests globally
+app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204);
+});
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
