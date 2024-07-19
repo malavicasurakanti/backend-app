fcd8e4c878bff4e58629595358b51a2ed69bbe4e
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-19 10:52:21 +0200

    Update server.js

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ dotenv.config();
 connectDB();
 const app = express();
 app.use(express.json());
-const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:3000'];
+const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:10000'];
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
@@ -53,6 +53,6 @@ app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(invalidPathHandler);
 app.use(errorResponserHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
