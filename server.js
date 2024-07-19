716b9970bf3db7dd860cc87282820902b4874bbe
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-19 17:06:39 +0200

    Update server.js

--- server.js
+++ server.js
@@ -38,22 +38,20 @@ app.use(cors({
       callback(new Error('Not allowed by CORS'));
     }
   },
-
-  app.use('*', cors());
-
-  app.use('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(204);
-});
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   preflightContinue: false,
   optionsSuccessStatus: 204
 }));
+
+ app.options('*', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(204);
+});
 
 app.get("/", (req, res) => {
   res.send("Server is running...");
