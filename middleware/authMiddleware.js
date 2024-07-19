3875651dab1f5e8f607b03d2bf67f499ec5f33c9
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-19 12:23:04 +0200

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,32 +3,29 @@ const { verify } = pkg;
 import User from "../models/User.js";
 
 export const authGuard = async (req, res, next) => {
+  console.log('Authorization Header:', req.headers.authorization);
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith('Bearer')
   ) {
     try {
-      const token = req.headers.authorization.split(" ")[1];
+      const token = req.headers.authorization.split(' ')[1];
+      console.log('Token:', token);
       const { id } = verify(token, process.env.JWT_SECRET);
-      req.user = await User.findById(id).select("-password");
+      console.log('User ID from Token:', id);
+      req.user = await User.findById(id).select('-password');
+      if (!req.user) {
+        throw new Error('User not found');
+      }
       next();
     } catch (error) {
-      let err = new Error("Not authorized, Token failed");
+      console.error('Token verification error:', error.message);
+      let err = new Error('Not authorized, Token failed');
       err.statusCode = 401;
       next(err);
     }
   } else {
-    let error = new Error("Not authorized, No token");
-    error.statusCode = 401;
-    next(error);
-  }
-};
-
-export const adminGuard = (req, res, next) => {
-  if (req.user && req.user.admin) {
-    next();
-  } else {
-    let error = new Error("Not authorized as an admn");
+    let error = new Error('Not authorized, No token');
     error.statusCode = 401;
     next(error);
   }
