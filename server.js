1eb2ef95965b433b4310ad12870206c5f2f70d6b
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-19 16:03:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -29,9 +29,6 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [process.env.FRONTEND_URL, 'http://localhost:10000', 'wwwdatavicacom.netlify.app'];
 
-
-const express = require('express');
-const cors = require('cors');
 app.use(cors({
   origin: function(origin, callback) {
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
