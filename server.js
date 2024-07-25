5b1090e6678f6058d154a2da0303d8bc7e130abc
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-25 19:39:45 +0200

    methods cors error

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ const app = express();
 app.use(express.json());
 app.use(cors( // Enable CORS
  {
-  origin: ['https://frontend-80y2vu8qe-malavicasurakantis-projects.vercel.app',]
-  methods:['POST','GET','PUT','DELETE']
+  origin: ['https://frontend-80y2vu8qe-malavicasurakantis-projects.vercel.app'],
+  methods:['POST','GET','PUT','DELETE'],
   credentials: true
 });
 
