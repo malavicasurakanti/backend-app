ec2903566822cc5dd3c6f078eb99f41be964c239
Malavica Surakanti <144533175+malavicasurakanti@users.noreply.github.com>
2024-07-26 10:32:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -53,5 +53,4 @@ app.use(invalidPathHandler);
 app.use(errorResponserHandler);
 }
 // Start the server
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
